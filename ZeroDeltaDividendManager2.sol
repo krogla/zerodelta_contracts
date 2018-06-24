0a1df1cb11966fbb38d9885dea63f517d899176d
KRogLA <i@krogla.me>
2018-06-24 10:05:30 +0200

    dividend manager (only eth)

---
+++ ZeroDeltaDividendManager2.sol
@@ -0,0 +1,190 @@
+pragma solidity ^0.4.24;
+
+library SafeMath {
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    /**
+    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor () public {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param _newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != address(0));
+        owner = _newOwner;
+        emit OwnershipTransferred(owner, _newOwner);
+    }
+}
+
+interface ZeroDeltaDividendToken {
+    function totalSupply() external view returns (uint256);
+    function decimals() external view returns (uint8);
+    function balanceOf(address who) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
+    function holders(uint256) external view returns (address);
+    function holdersCount() external view returns (uint256);
+
+    function totalSupplyAt(uint _blockNumber) external view  returns(uint);
+    function balanceOfAt(address _owner, uint _blockNumber) external view returns (uint);
+}
+
+
+contract ZeroDeltaDividendManager is Ownable {
+    using SafeMath for uint;
+
+    event DividendDeposited(address indexed _depositor, uint256 _blockNumber, uint256 _amount, uint256 _totalSupply, uint256 _dividendIndex);
+    event DividendClaimed(address indexed _claimer, uint256 _dividendIndex, uint256 _claim);
+    event DividendRecycled(address indexed _recycler, uint256 _blockNumber, uint256 _amount, uint256 _totalSupply, uint256 _dividendIndex);
+
+    ZeroDeltaDividendToken public dividendToken;
+
+    uint256 public RECYCLE_TIME = 365 days;
+
+    struct Dividend {
+        uint256 blockNumber;
+        uint256 timestamp;
+        uint256 amount;
+        uint256 claimedAmount;
+        uint256 totalSupply;
+        bool recycled;
+        mapping(address => bool) claimed;
+    }
+
+    Dividend[] public dividends;
+
+    mapping(address => uint256) dividendsClaimed;
+
+    modifier validDividendIndex(uint256 _dividendIndex) {
+        require(_dividendIndex < dividends.length);
+        _;
+    }
+
+    constructor (address _dividendToken) public {
+        dividendToken = ZeroDeltaDividendToken(_dividendToken);
+    }
+
+    function depositDividend() onlyOwner public  payable {
+        uint256 currentSupply = dividendToken.totalSupplyAt(block.number);
+        uint256 dividendIndex = dividends.length;
+        uint256 blockNumber = SafeMath.sub(block.number, 1);
+        dividends.push(
+            Dividend(
+                blockNumber,
+                now,
+                msg.value,
+                0,
+                currentSupply,
+                false
+            )
+        );
+        emit DividendDeposited(msg.sender, blockNumber, msg.value, currentSupply, dividendIndex);
+    }
+
+    function claimDividend(uint256 _dividendIndex) validDividendIndex(_dividendIndex) public {
+        Dividend storage dividend = dividends[_dividendIndex];
+        require(dividend.claimed[msg.sender] == false);
+        require(dividend.recycled == false);
+        uint256 balance = dividendToken.balanceOfAt(msg.sender, dividend.blockNumber);
+        uint256 claim = balance.mul(dividend.amount).div(dividend.totalSupply);
+        dividend.claimed[msg.sender] = true;
+        dividend.claimedAmount = SafeMath.add(dividend.claimedAmount, claim);
+        if (claim > 0) {
+            msg.sender.transfer(claim);
+            emit DividendClaimed(msg.sender, _dividendIndex, claim);
+        }
+    }
+
+    function claimDividendAll() public {
+        require(dividendsClaimed[msg.sender] < dividends.length);
+        for (uint i = dividendsClaimed[msg.sender]; i < dividends.length; i++) {
+            if ((dividends[i].claimed[msg.sender] == false) && (dividends[i].recycled == false)) {
+                dividendsClaimed[msg.sender] = SafeMath.add(i, 1);
+                claimDividend(i);
+            }
+        }
+    }
+
+    function recycleDividend(uint256 _dividendIndex) onlyOwner validDividendIndex(_dividendIndex) public {
+        Dividend storage dividend = dividends[_dividendIndex];
+        require(dividend.recycled == false);
+        require(dividend.timestamp < SafeMath.sub(now, RECYCLE_TIME));
+        dividends[_dividendIndex].recycled = true;
+        uint256 currentSupply = dividendToken.totalSupplyAt(block.number);
+        uint256 remainingAmount = SafeMath.sub(dividend.amount, dividend.claimedAmount);
+        uint256 dividendIndex = dividends.length;
+        uint256 blockNumber = SafeMath.sub(block.number, 1);
+        dividends.push(
+            Dividend(
+                blockNumber,
+                now,
+                remainingAmount,
+                0,
+                currentSupply,
+                false
+            )
+        );
+        emit DividendRecycled(msg.sender, blockNumber, remainingAmount, currentSupply, dividendIndex);
+    }
+}
+
